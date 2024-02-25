95975944022a6cbba8edecd5051a104dcf0304f2
Murmansk5000 <72131990+Murmansk5000@users.noreply.github.com>
2024-02-25 23:20:52 +0000

    v0.5.1

--- src/Baggage.java
+++ src/Baggage.java
@@ -1,8 +1,4 @@
-import AllException.FormatErrorException;
-import AllException.OverWeightException;
-
 public class Baggage {
-	// Assuming a base fee
 	private static final double BASE_FEE = 0.0;
 	private double length = 0.0;
 	private double width = 0.0;
@@ -37,6 +33,9 @@ public class Baggage {
 		this.height = height;
 		updateVolume();
 	}
+	public double getSize(){
+		return this.height + this.width + this.length;
+	}
 
 	private void updateVolume() {
 		this.volume = length * width * height;
@@ -62,27 +61,25 @@ public class Baggage {
 		this.fee = fee;
 	}
 
-	public void calculateFee() throws FormatErrorException {
-		// Reset the fee to a base value or specific initial charge
-		double weightFee = 0.0;
-		double sizeFee = 0.0;
-
-		double weightLimit = 23; // 23 kg weight limit
-		double sizeLimit = 158; // 158 cm size limit (length + width + height)
-		double excessWeightFee = 50; // Charge per kg for weight over the limit
-		double excessSizeCharge = 100; // Charge for size over the limit
-
-		// Check if the baggage is over the weight limit
-		if (weight > weightLimit) {
-			//weightFee += (weight - weightLimit) * excessWeightFee;
-			throw new FormatErrorException();
-		}
-
-		// Check if the baggage is over the size limit
-		if ((length + width + height) > sizeLimit) {
-			//sizeFee += excessSizeCharge;
-			throw new FormatErrorException();
-		}
-		fee = BASE_FEE + Math.max(weightFee, sizeFee);
-	}
+//	public void calculateFee() {
+//		// Reset the fee to a base value or specific initial charge
+//		double weightFee = 0.0;
+//		double sizeFee = 0.0;
+//
+//		double weightLimit = 23; // 23 kg weight limit
+//		double sizeLimit = 158; // 158 cm size limit (length + width + height)
+//		double excessWeightFee = 50; // Charge per kg for weight over the limit
+//		double excessSizeCharge = 100; // Charge for size over the limit
+//
+//		// Check if the baggage is over the weight limit
+//		if (weight > weightLimit) {
+//			weightFee += (weight - weightLimit) * excessWeightFee;
+//		}
+//
+//		// Check if the baggage is over the size limit
+//		if ((length + width + height) > sizeLimit) {
+//			sizeFee += excessSizeCharge;
+//		}
+//		fee = BASE_FEE + Math.max(weightFee, sizeFee);
+//	}
 }
